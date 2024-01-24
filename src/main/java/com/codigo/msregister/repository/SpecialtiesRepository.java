23910cbdd15fccba81aa823a23c8617eb50b41ea
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-01-24 17:59:09 -0500

    repositories implemented

---
+++ src/main/java/com/codigo/msregister/repository/SpecialtiesRepository.java
@@ -0,0 +1,7 @@
+package com.codigo.msregister.repository;
+
+import com.codigo.msregister.entity.SpecialtiesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecialtiesRepository extends JpaRepository<SpecialtiesEntity, Integer> {
+}
