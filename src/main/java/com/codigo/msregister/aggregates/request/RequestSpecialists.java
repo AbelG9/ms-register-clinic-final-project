d03ea83528b0cd7d044a9c79c7ab491ce84bed1d
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-02-01 15:13:22 -0500

    removed persons entity and methods

--- src/main/java/com/codigo/msregister/aggregates/request/RequestSpecialists.java
+++ src/main/java/com/codigo/msregister/aggregates/request/RequestSpecialists.java
@@ -1,7 +1,5 @@
 package com.codigo.msregister.aggregates.request;
 
-import com.codigo.msregister.entity.PersonsEntity;
-import com.codigo.msregister.entity.SpecialtiesEntity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
